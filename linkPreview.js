c7001912ac67ef7773cb7493e7f5484540cb2725
Adam Bernstein <abernstein@pih.org>
2017-12-06 10:36:36 -0500

    add popup framework

--- linkPreview.js
+++ linkPreview.js
@@ -2,10 +2,22 @@
 var commentLinks = document.querySelectorAll(".sitetable .usertext-body a");
 
 commentLinks.forEach(function(link) {
-  link.addEventListener("mouseover", function(){
-      //@TODO create popup
-  });
-
+    link.addEventListener("mouseover", createPopup);
+    link.addEventListener("mouseout", destroyPopup);
 });
 
+function createPopup(){
+    var popup = document.createElement("DIV");
+    var popText = document.createTextNode("Here's some text in a div");
+    popup.id = "rcotdDiv";
+    popup.className = "rcotd-popup";
+    popup.appendChild(popText);
+    this.parentElement.appendChild(popup);
+}
 
+function destroyPopup(){
+    var rcotd = document.querySelector("#rcotdDiv");
+    if (rcotd.parentNode) {
+        rcotd.parentNode.removeChild(rcotd);
+    }
+}
