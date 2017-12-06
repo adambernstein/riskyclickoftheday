6c87c068cdf7078d207f6efeedd7f9fdc8f5f6fc
Adam Bernstein <abernstein@pih.org>
2017-12-06 11:23:07 -0500

    tweak popups, working

--- linkPreview.js
+++ linkPreview.js
@@ -7,16 +7,20 @@ commentLinks.forEach(function(link) {
 });
 
 function createPopup(){
-    var popup = document.createElement("DIV");
-    var popText = document.createTextNode("Here's some text in a div");
+    var popupContainer = document.createElement("DIV"),
+        popup = document.createElement("DIV"),
+        popText = document.createTextNode("Here's some text in a div");
+    popupContainer.id = "rcotdContainer";
+    popupContainer.className = "rcotd-container";
     popup.id = "rcotdDiv";
     popup.className = "rcotd-popup";
+    popupContainer.appendChild(popup);
     popup.appendChild(popText);
-    this.parentElement.appendChild(popup);
+    this.parentElement.appendChild(popupContainer);
 }
 
 function destroyPopup(){
-    var rcotd = document.querySelector("#rcotdDiv");
+    var rcotd = document.querySelector("#rcotdContainer");
     if (rcotd.parentNode) {
         rcotd.parentNode.removeChild(rcotd);
     }
