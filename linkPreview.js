27a7528e5eb69f6b6a7fd99146b65b4335ec4e2c
Adam Bernstein <abernstein@pih.org>
2017-12-06 16:19:43 -0500

    request function needs rewrite

--- linkPreview.js
+++ linkPreview.js
@@ -9,14 +9,33 @@ commentLinks.forEach(function(link) {
 function createPopup(){
     var popupContainer = document.createElement("DIV"),
         popup = document.createElement("DIV"),
-        popText = document.createTextNode("Here's some text in a div");
+        popText = document.createTextNode("Here's some text in a div"),
+        targetUrl = this.attributes.href.value,
+        reqHeaders = new Headers();
+        reqHeaders.append('Content-Type', 'text/html')
+    var reqInit = { method: 'GET',
+                    headers: reqHeaders,
+                    mode: 'no-cors',
+                    cache: 'default'
+        },
+        reqObj = new Request(targetUrl, reqInit),
+        linkPreview = request(reqObj)
+            .then(function(response){
+                return response;
+            }).then(function(response){
+               return response;
+            }).catch(catchErr);
+
     popupContainer.id = "rcotdContainer";
     popupContainer.className = "rcotd-container";
     popup.id = "rcotdDiv";
     popup.className = "rcotd-popup";
     popupContainer.appendChild(popup);
     popup.appendChild(popText);
+    popup.appendChild(linkPreview);
     this.parentElement.appendChild(popupContainer);
+    //@TODO insert target doc (iframe? xhr?)
+
 }
 
 function destroyPopup(){
@@ -24,4 +43,24 @@ function destroyPopup(){
     if (rcotd.parentNode) {
         rcotd.parentNode.removeChild(rcotd);
     }
+}
+/*
+ * @TODO rewrite
+ */
+async function request(urlObj) {
+    const promise = await fetch(urlObj);
+    const data = await promise;
+    console.log(data);
+    return data;
+}
+
+function parseResponse(response) {
+    var parsedObj = JSON.parse(response);
+    console.log(parsedObj);
+    return parsedObj;
+}
+
+
+function catchErr(error){
+    console.log('Something went wrong: ', error);
 }
