ae42d2ed229fe8d8dac043893087d4a607217fc0
Adam Bernstein <abernstein@pih.org>
2017-12-07 16:23:43 -0500

    add license. broken linkpreview.js

--- linkPreview.js
+++ linkPreview.js
@@ -1,57 +1,59 @@
 /*Select links in user comments*/
-var commentLinks = document.querySelectorAll(".sitetable .usertext-body a");
+var commentLinks = document.querySelectorAll('.sitetable .usertext-body a');
 
 commentLinks.forEach(function(link) {
-    link.addEventListener("mouseover", createPopup);
-    link.addEventListener("mouseout", destroyPopup);
+    link.addEventListener('mouseover', createPopup);
+    link.addEventListener('mouseout', destroyPopup);
 });
 
 function createPopup(){
-    var popupContainer = document.createElement("DIV"),
-        popup = document.createElement("DIV"),
-        popText = document.createTextNode("Here's some text in a div"),
+    var popupContainer = document.createElement('DIV'),
+        popup = document.createElement('DIV'),
+        popText = document.createTextNode('Here\'s some text in a div'),
         targetUrl = this.attributes.href.value,
         reqHeaders = new Headers();
-        reqHeaders.append('Content-Type', 'text/html')
+    reqHeaders.append('Content-Type', 'text/html');
     var reqInit = { method: 'GET',
-                    headers: reqHeaders,
-                    mode: 'no-cors',
-                    cache: 'default'
+            headers: reqHeaders,
+            mode: 'no-cors',
+            cache: 'default'
         },
         reqObj = new Request(targetUrl, reqInit),
-        linkPreview = request(reqObj)
+        linkPreview = requestr(reqObj)
             .then(function(response){
                 return response;
             }).then(function(response){
-               return response;
+                return response;
             }).catch(catchErr);
 
-    popupContainer.id = "rcotdContainer";
-    popupContainer.className = "rcotd-container";
-    popup.id = "rcotdDiv";
-    popup.className = "rcotd-popup";
+    popupContainer.id = 'rcotdContainer';
+    popupContainer.className = 'rcotd-container';
+    popup.id = 'rcotdDiv';
+    popup.className = 'rcotd-popup';
+    this.parentElement.appendChild(popupContainer);
     popupContainer.appendChild(popup);
     popup.appendChild(popText);
-    popup.appendChild(linkPreview);
-    this.parentElement.appendChild(popupContainer);
+    //popup.appendChild(linkPreview);
+    
     //@TODO insert target doc (iframe? xhr?)
 
 }
 
 function destroyPopup(){
-    var rcotd = document.querySelector("#rcotdContainer");
+    var rcotd = document.querySelector('#rcotdContainer');
     if (rcotd.parentNode) {
         rcotd.parentNode.removeChild(rcotd);
     }
 }
+
 /*
  * @TODO rewrite
  */
-async function request(urlObj) {
-    const promise = await fetch(urlObj);
-    const data = await promise;
-    console.log(data);
-    return data;
+function requestr(reqObj){
+    fetch(reqObj).then(function(response) { return response.blob(); }).then(function(myBlob) {
+        console.log(myBlob);
+    });
+
 }
 
 function parseResponse(response) {
