bf356b3ba265b032fd6986ffaa9fdaca5e31cbed
Adam Bernstein <abernstein@pih.org>
2017-12-08 13:41:15 -0500

    popup create and destrory functions working

--- linkPreview.js
+++ linkPreview.js
@@ -1,17 +1,27 @@
 /*Select links in user comments*/
-var commentLinks = document.querySelectorAll('.sitetable .usertext-body a');
-var responseText = '';
+var commentLinks = document.querySelectorAll('.sitetable .usertext-body a'),
+    responseText = '';
+    
 
 commentLinks.forEach(function(link) {
-    link.addEventListener('mouseover', createPopup);    
+    link.addEventListener('mouseover', initPopup);    
 });
 
-function createPopup(){
+function initPopup(){
+    let rcotd = document.getElementById('rcotdContainer');
+    if (rcotd!=null) {
+        destroyPopup();
+        buildPopup(this);
+    }
+    else buildPopup(this);
+}
+
+function buildPopup(elem){
     var popupContainer = document.createElement('DIV'),
         popup = document.createElement('DIV'),
         popText = document.createTextNode('Here\'s some text in a div\n'),
         popResponseText = document.createTextNode(responseText),
-        targetUrl = this.attributes.href.value;
+        targetUrl = elem.attributes.href.value;
     
     //define Request object
     var reqHeaders = new Headers();
@@ -35,20 +45,20 @@ function createPopup(){
     popupContainer.className = 'rcotd-container';
     popup.id = 'rcotdDiv';
     popup.className = 'rcotd-popup';
-    this.parentElement.appendChild(popupContainer);
+    elem.parentElement.appendChild(popupContainer);
     popupContainer.appendChild(popup);
     popup.appendChild(popText);
     popup.appendChild(popResponseText);
     
-    document.querySelector('.rcotd-container').addEventListener('click', destroyPopup);
+    document.querySelector('body').addEventListener('click', destroyPopup);
     //@TODO insert target doc (iframe? xhr?)
 
 }
 
 function destroyPopup(){
-    var rcotd = document.querySelector('#rcotdContainer');
-    if (rcotd.parentNode) {
-        rcotd.parentNode.removeChild(rcotd);
+    let rcotdElement = document.getElementById('rcotdContainer');
+    if (rcotdElement) {
+        rcotdElement.parentNode.removeChild(rcotdElement);
     }
 }
 
